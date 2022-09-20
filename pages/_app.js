f2bc06d30c4d127ddf6659d7fa8c0658aafc0a0b
furky.eth <52575351+furkananter@users.noreply.github.com>
2022-09-21 02:13:31 +0300

    dashboard walletconnect

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,54 @@
 import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { alchemyProvider } from 'wagmi/providers/alchemy'
 
+const { chains, provider } = configureChains(
+  [chain.mainnet],
+  [alchemyProvider({ apiKey: 'ZDETcvU9gPHmA8jSypsOMgG1FPxb30i1' })],
+)
+
+const connectors = [
+  new CoinbaseWalletConnector({
+  options: {
+    appName: 'wagmi.sh',
+    jsonRpcUrl: 'https://eth-mainnet.g.alchemy.com/v2/ZDETcvU9gPHmA8jSypsOMgG1FPxb30i1',
+  },
+}),
+new MetaMaskConnector(
+  {
+    options: {
+      rpc: {
+        1: 'https://eth-mainnet.g.alchemy.com/v2/ZDETcvU9gPHmA8jSypsOMgG1FPxb30i1',
+      },
+    },
+  },
+),
+new WalletConnectConnector({
+  options: {
+    rpc: {
+      2: 'https://eth-mainnet.g.alchemy.com/v2/ZDETcvU9gPHmA8jSypsOMgG1FPxb30i1',
+    },
+  },
+}),
+]
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-      <ChakraProvider>
+    <ChakraProvider>
+      <WagmiConfig client={wagmiClient}>
         <Component {...pageProps} />
-      </ChakraProvider>
+      </WagmiConfig>
+    </ChakraProvider>
   )
 }
 
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,27 +1,92 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import Navbar from './components/Navbar'
-import { useEffect, useState } from 'react'
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
+
 
 
 // 1. Handle the Wallet Connection 
-  // use 
+// use 
 // 2. Fetch the NFTs from the collection
 // 3. Display the NFTs in the dashboard
 
-function fetchData(id){
+function fetchData(id) {
   fetch(`https://api.opensea.io/api/v1/`)
-  .then((response) => response.json())
-  .then((data) => console.log(data))
+    .then((response) => response.json())
+    .then((data) => console.log(data))
 }
 
-const Dashboard = () => { 
+const Dashboard = () => {
+  const { connector: activeConnector, isConnected, address } = useAccount()
+  const { connect, connectors, error, isLoading, pendingConnector } =
+    useConnect()
+  const { disconnect } = useDisconnect()
+
   return (
-    <div >
-      <button onClick={() => fetchData(4)}> Fetch Data</button>
-       <p></p>
-    </div>
+    <>
+      {isConnected &&
+        <div>
+              <span>
+                
+              Connected to {activeConnector.name}
+              </span>
+            <span> 
+              Connected to with address {address}
+              </span>
+          
+            <button onClick={disconnect}>Disconnect</button>
+
+        </div>
+      }
+      {!isConnected &&
+        connectors.map((connector) => (
+          <div key={connector.id}>
+            <button
+              className='bg-black text-white p-4 w-1/5 cursor-pointer'
+              onClick={() => connect({ connector })}
+              disabled={isLoading || pendingConnector === connector.id}
+            >
+              {connector.name}
+              {isLoading &&
+                pendingConnector?.id === connector.id &&
+                ' (connecting)'}
+            </button>
+
+          </div>
+        ))
+      }
+      {error && <div>{error.message}</div>}
+    </>
   )
 }
 
 export default Dashboard
+
+{/*
+
+
+
+
+
+  
+  const { connect, connectors } = useConnect();
+  const { address, isConnecting, isDisconnected } = useAccount()
+  const { disconnect } = useDisconnect();
+
+  return (
+    <div className="App">
+    <button onClick={() => { connect({ connector: connectors[0] }) }}>Coinbase Wallet</button>
+    <hr/>
+    <button onClick={() => { connect({ connector: connectors[1] }) }}>Metamask</button>
+    <hr/>
+    <button onClick={() => { connect({ connector: connectors[2] }) }}>Wallet Connect</button>
+
+    <hr/>
+    <button onClick={disconnect}>Disconnect</button>
+
+    <div>
+      {isConnecting && <p>Connecting...</p>}
+      {isDisconnected && <p>Disconnected</p>}
+      <p>{}</p>
+    </div>
+  </div>
+)
+
+*/}
